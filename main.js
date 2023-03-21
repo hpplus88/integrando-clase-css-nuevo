a9da62093aaa7ca56843dc41976b77f75f488f8d
hpplus88 <hernanpelatti@gmail.com>
2023-03-21 18:28:12 -0300

    Hicimos responsive y agregamos menu en desktop y mobile

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+
+
 
 function toggleDesktopMenu(){
+    //el toggle agrega o quita la etiqueta inactive
     desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+};
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
 };
